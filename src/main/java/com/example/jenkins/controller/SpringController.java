a5563bab452224e5b84bf3117bce6621d381e66d
saikumardasari8888 <saikumardasari8179@gmail.com>
2023-08-17 22:13:20 +0530

    initial commit

--- src/main/java/com/example/jenkins/controller/SpringController.java
+++ src/main/java/com/example/jenkins/controller/SpringController.java
@@ -2,6 +2,7 @@ package com.example.jenkins.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class SpringController {
     @GetMapping("/getMessage")
     public String getMessage(){
         return "hello world hello";
+    }
+
+    @GetMapping("/greating/{name}")
+    public String Message(@PathVariable("name") String name){
+        return "hello "+name;
     }
 }
