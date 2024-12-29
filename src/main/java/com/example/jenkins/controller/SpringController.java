2481adbfacfd38382723333f317b130fa6446a9a
Nani <malothunaveen216@.com>
2024-12-29 10:59:03 +0530

    added pipeline

--- src/main/java/com/example/jenkins/controller/SpringController.java
+++ src/main/java/com/example/jenkins/controller/SpringController.java
@@ -7,41 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringController {
-
-
-    @GetMapping("/getMessage")
+    @GetMapping("/getSai")
     public String getMessage(){
     return "hello world hello   ";
-    }
-
-    @GetMapping("/greating/{name}")
-    public String Message(@PathVariable("name") String name){
-        return "hello "+name;
-    }
-
-    @GetMapping("/message")
-    public String postMessage(){
-        return "hello world";
-    }
-    @GetMapping("/saikumar")
-    public String saiMessage(){
-        return "hello saikumar";
-    }
-
-
-    @GetMapping("/hello")
-    public String helloMessage(){
-        return "hello message";
-    }
-
-    @GetMapping("/gali")
-    public String galiMessage(){
-        return "gali message";
-    }
-
-
-    @GetMapping("/getNaveen/{name}")
-    public String NaveenMessage(@PathVariable("name") String name){
-        return name + " message";
     }
 }
