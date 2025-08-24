b14c556dca33e5a5c126f1278ee5e90d81a9f2f9
saikumardasari8888 <137864713+saikumardasari8888@users.noreply.github.com>
2025-08-24 23:14:54 +0530

    Update JenkinsApplication.java

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//test
 @SpringBootApplication
 public class JenkinsApplication extends SpringBootServletInitializer  {
 
